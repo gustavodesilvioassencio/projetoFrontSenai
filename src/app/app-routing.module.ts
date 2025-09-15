d5619e1046e5cb3e260904d369c06ff823f0a586
vagnerantunes <vagner.antunes94@gmail.com>
2025-09-15 14:09:38 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './views/home/home.component';
 import {ProductCrudComponent} from  './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
-import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
-import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
-import { FormaPagamentoCrudComponent } from './views/forma-pagamento-crud/forma-pagamento-crud.component';
-import { FornecedorCrudComponent } from './views/fornecedor-crud/fornecedor-crud.component';
-import { FornecedorCreateComponent } from './components/fornecedor/fornecedor-create/fornecedor-create.component';
 
 //configuração para rotear entre as paginas na home
 const routes: Routes = [
@@ -22,26 +17,6 @@ const routes: Routes = [
   {
     path: "products/create",
     component: ProductCreateComponent
-  },
-  {
-    path: "products/update/:proId",
-    component: ProductUpdateComponent
-  },
-  {
-    path: "products/delete/:proId",
-    component: ProductDeleteComponent
-  },
-  {
-    path: "fpagamentos",
-    component: FormaPagamentoCrudComponent
-  },
-  {
-    path: "fornecedores",
-    component: FornecedorCrudComponent
-  },
-  {
-    path: "fornecedores/create",
-    component: FornecedorCreateComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,14 +9,14 @@ import { HeaderComponent } from './components/template/header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 //pegar http 
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
@@ -30,16 +29,8 @@ import { RedDirective } from './directives/red.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
 //importações
-import { ProductReadComponent } from './components/product/product-read/product-read.component';
-
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
-import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
-import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
-import { FormaPagamentoReadComponent } from './components/formaPagamento/forma-pagamento-read/forma-pagamento-read.component';
-import { FormaPagamentoCrudComponent } from './views/forma-pagamento-crud/forma-pagamento-crud.component';
-import { FornecedorCrudComponent } from './views/fornecedor-crud/fornecedor-crud.component';
-import { FornecedorCreateComponent } from './components/fornecedor/fornecedor-create/fornecedor-create.component';
 
 registerLocaleData(localePt)
 
@@ -52,21 +43,13 @@ registerLocaleData(localePt)
     HomeComponent,
     ProductCrudComponent,
     RedDirective,
-    ProductCreateComponent,
-    ProductReadComponent,
-    ProductUpdateComponent,
-    ProductDeleteComponent,
-    FormaPagamentoReadComponent,
-    FormaPagamentoCrudComponent,
-    FornecedorCrudComponent,
-    FornecedorCreateComponent
-  ],
+    ProductCreateComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     //precisamos declara os modulos de material importados
-    MatToolbarModule, 
+    MatToolbarModule,
     MatSidenavModule,
     MatListModule,
     MatCardModule,
